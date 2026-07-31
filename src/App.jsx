c1f6d9f882a4f0ac08371f2af44f845656e0650b
jagmohan7 <jagmohan@printdeed.com>
2026-07-31 18:06:45 +0530

    Fix case-sensitive import and restore vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import TechnologiesPage from "./pages/TechnologiesPage.jsx";
+import TechnologiesPage from "./pages/technologiesPage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
